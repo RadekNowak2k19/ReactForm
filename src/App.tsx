dca7e2e5a42745e5c4d1e1bdee99ab3a9e7965ad
Radosław Nowak <radeknowak2k19@gmail.com>
2023-11-28 21:48:16 +0100

    Created main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,45 @@
+import styles from "./App.module.scss";
 export const App = () => {
-	return <div>App</div>;
+	return (
+		<div className={styles.formElement}>
+			<div className={styles.formElement_steps}>Steps</div>
+			<div className={styles.formElement_stepsData}>
+				<div className={styles.fillBackground}>
+					<div className={styles.fillContainer}>
+						<div>
+							<h2>Personal info</h2>
+							<p>Please provide your name, email, address, and phone number.</p>
+						</div>
+						<form>
+							<div className={styles.inputForm}>
+								<div>
+									<label htmlFor="">Name</label>
+									<div>error</div>
+								</div>
+								<input type="text" placeholder="Name" />
+							</div>
+							<div className={styles.inputForm}>
+								<div>
+									<label htmlFor="">Name</label>
+									<div>error</div>
+								</div>
+								<input type="text" placeholder="Name" />
+							</div>
+							<div className={styles.inputForm}>
+								<div>
+									<label htmlFor="">Name</label>
+									<div>error</div>
+								</div>
+								<input type="text" placeholder="Name" />
+							</div>
+						</form>
+					</div>
+				</div>
+				<div className={styles.buttons}>
+					<button>Go Back</button>
+					<button>NextStep</button>
+				</div>
+			</div>
+		</div>
+	);
 };
---
+++ src/assets/images/index.ts
@@ -0,0 +1,17 @@
+import sidebarMobile from "./sidebarMobile.svg";
+import sidebarDesktop from "./sidebarDesktop.svg";
+import iconAdvanced from "./iconAdvanced.svg";
+import iconArcade from "./iconArcade.svg";
+import iconCheckmark from "./iconCheckmark.svg";
+import iconPro from "./iconPro.svg";
+import iconThank from "./iconThank.svg";
+
+export {
+	sidebarDesktop,
+	sidebarMobile,
+	iconAdvanced,
+	iconArcade,
+	iconCheckmark,
+	iconPro,
+	iconThank,
+};
