503849a0a25986f221605c1053942bc67210319e
Radosław Nowak <radeknowak2k19@gmail.com>
2023-11-30 19:27:34 +0100

    Created Heading element

---
+++ src/ui/Heading.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import styles from "./Heading.module.scss";
+
+type HeadingProps = {
+	headingProps: { title: string; paragraph: string };
+};
+
+export const Heading: FC<HeadingProps> = ({ headingProps }) => {
+	console.log(headingProps);
+	return (
+		<div className={styles.heading}>
+			<h2>{headingProps.title}</h2>
+			<p>{headingProps.paragraph}</p>
+		</div>
+	);
+};
