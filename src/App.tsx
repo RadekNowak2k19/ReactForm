23e6b489b56ae101fedd545a5c95ec4c25cf5c8b
Radosław Nowak <radeknowak2k19@gmail.com>
2023-12-04 19:33:50 +0100

    Created StepFour element

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { StepOne } from "./components/StepOne";
 import { StepTwo } from "./components/StepTwo";
 import { StepThree } from "./components/StepThree";
+import { StepsFour } from "./components/StepFour";
 
 export const App = () => {
 	return (
@@ -8,6 +9,7 @@ export const App = () => {
 			{/* <StepOne /> */}
 			{/* <StepTwo /> */}
 			{/* <StepThree /> */}
+			<StepsFour />
 		</div>
 	);
 };
